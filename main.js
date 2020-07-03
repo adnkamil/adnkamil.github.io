10eb0f7d287aa1f37c723f7a2e038dfb63f609d9
adnkamil <adnankamil27@gmail.com>
2020-07-03 16:14:09 +0800

    Yokk bissa yokk

---
+++ main.js
@@ -0,0 +1,6 @@
+const exit = document.getElementById('start')
+
+exit.addEventListener('mouseover', function () {
+    alert('Maaf, untuk saat ini GAME masih dalam pengembangan jadi diharapkan kembali di lain waktu');
+    
+});
